e3d723cee353d52a4dd6f2f0dc1d5233366cf8ad
herreang000 <herreang000@warren.k12.in.us>
2025-02-05 08:30:38 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const counterElement  = document.getElementById("counter");
+let count = +counterElement.innerText;
+
+const increment = () => {
+    count++
+    counterElement.innerText = count;
+};
+
+const incrementButton = document.getElementById("increment");
+incrementButton.addEventListener("click", increment);
+
+const decrement = () => {
+    count--
+    counterElement.innerText = count;
+};
+
+const decrementButton = document.getElementById("decrement");
+decrementButton.addEventListener("click", decrement);
